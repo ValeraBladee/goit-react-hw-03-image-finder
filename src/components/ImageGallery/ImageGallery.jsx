6e30cfa9f3176c27ca0b9000c591327912d5e74d
ValeraBladee <potniyschenok@gmail.com>
2023-10-31 20:19:47 +0200

    +

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import css from './ImageGallery.module.css';
+
+// export default function ImageGallery({ cards, getBigPhoto }) {
+//   return (
+//     <ul className={css.ImageGallery}>
+//       {cards.map(({ webFormatUrl, largeImageURL, tags }) => {
+       
+//       })}
+//     </ul>
+//   );
+// }
