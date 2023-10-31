ab373a35ade7451b35a53d8438dfb59ea2a7354a
ValeraBladee <potniyschenok@gmail.com>
2023-10-31 21:21:11 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,107 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loader from './Loader/Loader';
+import Modals from './Modal/Modal';
+
+import getImage from '../api-ferch';
+
+class App extends Component {
+  state = {
+    searchPhoto: '',
+    page: 0,
+    totalPages: '',
+    totalHits: '',
+    photo: [],
+    loading: false,
+    error: null,
+    bigUrl: null,
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { searchPhoto, page } = this.state;
+
+    if (prevState.searchPhoto !== searchPhoto || prevState.page !== page) {
+      this.setState({ error: null, loading: true });
+
+      getImage(searchPhoto, page)
+        .then(data => {
+          this.setState({
+            totalHits: data.totalHits,
+            totalPages: Math.ceil(data.totalHits / 12),
+          });
+          this.setState(prevState => ({
+            photo: [...prevState.photo, ...data.hits],
+          }));
+        })
+        .catch(error => this.setState({ error: error }))
+        .finally(() => {
+          this.setState({ loading: false });
+        });
+    }
+  }
+
+  getBigUrlPhoto = evt => {
+    const bigPhotoId = evt.currentTarget.getAttribute('id');
+    this.setState({ bigUrl: bigPhotoId });
+  };
+
+  onClick = event => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+      loading: true,
+    }));
+  };
+
+  settingSearchOption = option => {
+    const { searchPhoto, totalPages, page } = this.state;
+
+    if (searchPhoto === option) {
+      if (page === totalPages) {
+        this.setState({ photo: [], searchPhoto: option, page: 1 });
+        return;
+      }
+      this.setState(prevState => ({
+        page: prevState.page + 1,
+        loading: true,
+      }));
+
+      return;
+    }
+
+    this.setState({
+      photo: [],
+      searchPhoto: option,
+      totalPages: '',
+      page: 1,
+      totalHits: '',
+    });
+  };
+
+  render() {
+    const { photo, totalPages, page, loading, bigUrl, error } = this.state;
+    const dieRenderingBtn = totalPages > page;
+    console.log(this.state);
+    return (
+      <>
+        <Searchbar onSubmit={this.settingSearchOption} />
+        {photo && photo.length > 0 && (
+          <ImageGallery getBigPhoto={this.getBigUrlPhoto} cards={photo} />
+        )}
+        {loading && <Loader />}
+        {dieRenderingBtn && <Button onClick={this.onClick} />}
+        {bigUrl && (
+          <Modals
+            url={bigUrl}
+            onClose={() => this.setState({ bigUrl: null })}
+          />
+        )}
+        {error && error}
+      </>
+    );
+  }
+}
+
+export default App;
